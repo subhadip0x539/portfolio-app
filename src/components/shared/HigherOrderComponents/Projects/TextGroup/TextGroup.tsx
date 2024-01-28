0a81d59d7f0d10f5cb21d91cc16b4ff333247590
subhadip0x539 <biswassubhadip0x539@gmail.com>
2024-01-29 00:11:43 +0530

    🎨 refactor(background): change all bg-background classes to bg-background-primary

--- src/components/shared/HigherOrderComponents/Projects/TextGroup/TextGroup.tsx
+++ src/components/shared/HigherOrderComponents/Projects/TextGroup/TextGroup.tsx
@@ -1,7 +1,7 @@
 export function TextGroup() {
   return (
     <div className="flex flex-col gap-4 max-w-prose text-center  items-center">
-      <h2 className="sm:text-5xl text-4xl alt-font-family font-bold text-clip ">
+      <h2 className="sm:text-5xl text-4xl alt-font-family font-bold text-clip">
         Things I've built
       </h2>
     </div>
